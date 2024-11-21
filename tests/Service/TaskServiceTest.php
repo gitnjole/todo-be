574cd5a0b3fa38dca45b1eef2e8a20748551ed92
Antonio Cahun <cahunantonio@gmail.com>
2024-11-21 10:00:34 +0100

    test: refactor tests to reflect repository pattern usage

--- tests/Service/TaskServiceTest.php
+++ tests/Service/TaskServiceTest.php
@@ -6,24 +6,20 @@ use App\Entity\Task;
 use App\Entity\User;
 use App\Repository\TaskRepository;
 use App\Service\TaskService;
-use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class TaskServiceTest extends TestCase
 {
-    private EntityManagerInterface|MockObject $entityManagerMock;
     private TaskRepository|MockObject $taskRepositoryMock;
     private TaskService $taskService;
     private User $user;
 
     protected function setUp(): void
     {
-        $this->entityManagerMock = $this->createMock(EntityManagerInterface::class);
         $this->taskRepositoryMock = $this->createMock(TaskRepository::class);
 
         $this->taskService = new TaskService(
-            $this->entityManagerMock,
             $this->taskRepositoryMock
         );
 
@@ -39,8 +35,8 @@ class TaskServiceTest extends TestCase
 
         $this->taskRepositoryMock
             ->expects($this->once())
-            ->method('findBy')
-            ->with(['user_task' => $this->user])
+            ->method('findByUser')
+            ->with($this->user)
             ->willReturn($tasks);
 
         $result = $this->taskService->fetchByUser($this->user);
@@ -56,18 +52,14 @@ class TaskServiceTest extends TestCase
     {
         $task = new Task();
 
-        $this->entityManagerMock
+        $this->taskRepositoryMock
             ->expects($this->once())
-            ->method('persist')
+            ->method('create')
             ->with($this->callback(function (Task $persistedTask) use ($task) {
                 $this->assertSame($this->user, $persistedTask->getUserTask());
                 $this->assertInstanceOf(\DateTimeImmutable::class, $persistedTask->getCreatedAt());
                 return true;
             }));
-
-        $this->entityManagerMock
-            ->expects($this->once())
-            ->method('flush');
 
         $this->taskService->create($task, $this->user);
     }
@@ -80,9 +72,10 @@ class TaskServiceTest extends TestCase
         $task = new Task();
         $task->setTitle('Updated Title');
 
-        $this->entityManagerMock
+        $this->taskRepositoryMock
             ->expects($this->once())
-            ->method('flush');
+            ->method('update')
+            ->with($task);
 
         $this->taskService->update($task);
     }
@@ -94,14 +87,10 @@ class TaskServiceTest extends TestCase
     {
         $task = new Task();
 
-        $this->entityManagerMock
+        $this->taskRepositoryMock
             ->expects($this->once())
-            ->method('remove')
+            ->method('delete')
             ->with($task);
-
-        $this->entityManagerMock
-            ->expects($this->once())
-            ->method('flush');
 
         $this->taskService->delete($task);
     }
--- tests/Service/UserServiceTest.php
+++ tests/Service/UserServiceTest.php
@@ -3,26 +3,26 @@
 namespace App\Tests\Service;
 
 use App\Entity\User;
-use App\Service\RegistrationService;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
+use App\Service\UserService;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class RegistrationServiceTest extends TestCase
+class UserServiceTest extends TestCase
 {
     private UserPasswordHasherInterface|MockObject $passwordHasherMock;
-    private EntityManagerInterface|MockObject $entityManagerMock;
-    private RegistrationService $registrationService;
+    private UserRepository|MockObject $userRepositoryMock;
+    private UserService $userService;
 
     protected function setUp(): void
     {
         $this->passwordHasherMock = $this->createMock(UserPasswordHasherInterface::class);
-        $this->entityManagerMock = $this->createMock(EntityManagerInterface::class);
+        $this->userRepositoryMock = $this->createMock(UserRepository::class);
 
-        $this->registrationService = new RegistrationService(
+        $this->userService = new UserService(
             $this->passwordHasherMock,
-            $this->entityManagerMock
+            $this->userRepositoryMock
         );
     }
 
@@ -41,22 +41,17 @@ class RegistrationServiceTest extends TestCase
             ->with($user, $plainPassword)
             ->willReturn($hashedPassword);
 
-        $this->entityManagerMock
+        $this->userRepositoryMock
             ->expects($this->once())
-            ->method('persist')
+            ->method('create')
             ->with($this->callback(function (User $persistedUser) use ($hashedPassword) {
                 $this->assertEquals($hashedPassword, $persistedUser->getPassword());
-
                 $this->assertEquals(['ROLE_USER'], $persistedUser->getRoles());
 
                 return true;
             }));
 
-        $this->entityManagerMock
-            ->expects($this->once())
-            ->method('flush');
-
-        $this->registrationService->registerUser($user, $plainPassword);
+        $this->userService->create($user, $plainPassword);
     }
 
     /**
@@ -74,8 +69,8 @@ class RegistrationServiceTest extends TestCase
             ->with($user, $plainPassword)
             ->willReturn($hashedPassword);
 
-        $reflectionMethod = new \ReflectionMethod($this->registrationService, 'registerUser');
-        $reflectionMethod->invoke($this->registrationService, $user, $plainPassword);
+        $reflectionMethod = new \ReflectionMethod($this->userService, 'create');
+        $reflectionMethod->invoke($this->userService, $user, $plainPassword);
 
         $this->assertEquals($hashedPassword, $user->getPassword());
     }
@@ -92,7 +87,7 @@ class RegistrationServiceTest extends TestCase
             ->method('hashPassword')
             ->willReturn('hashedPassword');
 
-        $this->registrationService->registerUser($user, $plainPassword);
+        $this->userService->create($user, $plainPassword);
 
         $this->assertEquals(['ROLE_USER'], $user->getRoles());
     }
